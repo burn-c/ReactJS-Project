2fbc6dcd9d1f51ef73968c2d9708337a58f1fcd5
burn-c <chopcsom@gmail.com>
2019-11-07 20:00:49 -0300

    Config: API->LoadingData

--- module05/src/pages/Repository/index.js
+++ module05/src/pages/Repository/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repositories)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repositories: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repositories);
+
+    const [repositories, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`),
+      {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      },
+    ]);
+
+    this.setState({
+      repositories: repositories.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repositories, issues, loading } = this.state;
+    return <h1>Repository</h1>;
+  }
 }
